a91fa02d4f86877047360fccfc5e19de82d70aab
Octavia Couneson <octavia.couneson@gmail.com>
2021-01-31 17:23:47 +0100

    Working docker environment with nginx

---
+++ code/index.php
@@ -0,0 +1,18 @@
+<?php
+#$url = isset($_SERVER['PATH_INFO']) ? explode('/', $_SERVER['PATH_INFO']) : '/';
+
+#var_dump($url);
+
+echo "This is PHP";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<p>Hello world</p>
+</body>
+</html>
